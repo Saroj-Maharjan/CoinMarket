7d17c6c52676d94d3ad67b19c1182186d341fa59
saroj maharjan <saroj.maharjan1993@gmail.com>
2024-12-14 09:18:13 +0800

    Update Version

--- app/src/main/java/com/sawrose/cryptotracker/di/AppModule.kt
+++ app/src/main/java/com/sawrose/cryptotracker/di/AppModule.kt
@@ -5,8 +5,8 @@ import com.sawrose.cryptotracker.crypto.data.networking.RemoteCoinDataSource
 import com.sawrose.cryptotracker.crypto.domain.CoinDataSource
 import com.sawrose.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
 import io.ktor.client.engine.cio.CIO
-import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
